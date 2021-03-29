a86b40871c4ed7dee3bfc4347d6a615110247a95
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-03-29 14:58:27 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,34 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
+  const [query, setQuery] = useState('')
 
   const fetchImages = async () => {
     setLoading(true)
-    let url = `${mainUrl}${clientID}`
+    let url
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+
     try {
       const response = await fetch(url)
       const data = await response.json()
       // console.log(data)
-      setPhotos(data)
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
       setLoading(false)
     } catch (error) {
       setLoading(false)
@@ -27,22 +46,43 @@ function App() {
 
   useEffect(() => {
     fetchImages()
-  }, [])
+  }, [page])
 
   useEffect(() => {
-    const event = window.addEventListener('scroll', () => {})
+    const event = window.addEventListener('scroll', () => {
+      // console.log(`innerHeight ${window.innerHeight}`)
+      // console.log(`scrollY ${window.scrollY}`)
+      // console.log(`body height ${document.body.scrollHeigthy}`)
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        // console.log('we are at the end of the document')
+        setPage((oldPage) => {
+          return oldPage + 1
+        })
+      }
+    })
     return () => window.removeEventListener('scroll', event)
   }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
     // console.log('hello')
+    setPage(() => 1)
+    fetchImages()
   }
   return (
     <main>
       <section className='search'>
         <form className='search-form'>
-          <input type='text' placeholder='search' className='form-input' />
+          <input
+            type='text'
+            placeholder='search'
+            className='form-input'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type='submit' className='submit-btn' onClick={handleSubmit}>
             <FaSearch />
           </button>
